aa506a7b6e518bdd98736dbdee64eef8cce1c31a
sandeepsharma12021994 <er.sandeepsharma12021994@gmail.com>
2018-02-04 18:01:42 +0530

    t

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ var app=express();
 var db
 var http=require('http')
 app.use(bodyparser.urlencoded({extended:true}))
-app.use(express.static('public'))
-app.use(bodyparser.json())
 app.set('view engine', 'ejs',(req,res)=>{
 	res.render(view, locals)
 })
@@ -30,30 +28,6 @@ app.post('/quotes',(req,res) =>{
     console.log('saved to database')
     res.redirect('/')
   })
-})
-
-app.put('/quotes', (req, res) => {
-  db.collection('quotes')
-  .findOneAndUpdate({name: 'Yoda'}, {
-    $set: {
-      name: req.body.name,
-      quote: req.body.quote
-    }
-  }, {
-    sort: {_id: -1},
-    upsert: true
-  }, (err, result) => {
-    if (err) return res.send(err)
-    res.send(result)
-  })
-  fetch({ /* request */ })
-.then(res => {
-  if (res.ok) return res.json()
-})
-.then(data => {
-  console.log(data)
-  window.location.reload(true)
-})
 })
 
 app.listen(3000,function()
