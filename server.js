3aedb6205d41ed9b630a6d88229a9d9ae5c41943
sandeepsharma12021994 <er.sandeepsharma12021994@gmail.com>
2018-02-04 17:32:57 +0530

    save and get

--- server.js
+++ server.js
@@ -6,35 +6,28 @@ var db
 var http=require('http')
 app.use(bodyparser.urlencoded({extended:true}))
 
-
+app.set('view engine', 'ejs',(req,res)=>{
+	res.render(view, locals)
+})
 
 MongoClient.connect('mongodb://localhost:27017/', (err, client) => {
 if(err) return console.log(err)
 	db = client.db('mydbtest') // whatever your database name is
 })
 
-app.get('/',(req,res,next) =>{
-	res.sendFile(__dirname +'/index.html')
-	// Note: __dirname is directory that contains the JavaScript source code. Try logging it and see what you get!
-  // Mine was '/Users/administrator/Document/' for this app.
-})
-// // app.get('/quotes',function(req,res,next){
-	// // res.setheader("content-type","text/html");
-	// // res.sendfile(__dirname +'/index.html')
-// // })
-app.get('/thanks',(req,res,next) =>{
-	res.send('Thanks for posting a new quote.')
-	// Note: __dirname is directory that contains the JavaScript source code. Try logging it and see what you get!
-  // Mine was '/Users/administrator/Document/' for this app.
-})
-app.post('/quotes',(req,res,next) =>{
-	res.setHeader("Content-type","text/html");
-	db.collection('quotes').save(req.body, (err, result) => {
+app.get('/', (req, res) => {
+  db.collection('quotes').find().toArray((err, result) => {
     if (err) return console.log(err)
-    
+    // renders index.ejs
+    res.render('index.ejs', {quotes: result})
+  })
+})	
+
+app.post('/quotes',(req,res) =>{	
+	db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err)    
     console.log('saved to database')
-	res.redirect('/thanks')
-   // res.send('saved to database')
+    res.redirect('/')
   })
 })
 
